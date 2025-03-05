e573b8d6785a051d5d0e89529edc520719da5238
jill bhatt <bhattjill@gmail.com>
2025-03-05 17:36:41 +0530

    Added debug log in index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const initExpressApolloApp = require('./app');
 const { PORT } = require('./config/env');
 const { connectMongoDB } = require('./db');
+const CustomError = require('./shared/CustomError');
+
+console.log('CustomError: ', CustomError);
 
 connectMongoDB()
     .then(async conn => {
