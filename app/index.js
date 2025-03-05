34c0f4aa28e6e71dfcb2d79e66c46dfe4fc8ad09
jill bhatt <bhattjill@gmail.com>
2025-03-05 16:52:50 +0530

    Updated CustomError import in user mutation

--- app/index.js
+++ app/index.js
@@ -34,7 +34,6 @@ const initExpressApolloApp = async () => {
     const apolloServer = new ApolloServer({
         schema,
         introspection: true,
-        playground: true,
         formatError: error => {
             return {
                 message: error.message,
--- schema/mutation/users/index.js
+++ schema/mutation/users/index.js
@@ -1,7 +1,7 @@
 const { GraphQLNonNull, GraphQLBoolean } = require('graphql');
 const { hash: hashPassword, verify: verifyPassword } = require('argon2');
 const UserModel = require('../../../models/User');
-const CustomError = require('../../../common/error');
+const CustomError = require('../../../common/error/index');
 const {
     LoginUserInputType,
     SignupUserInputType,
