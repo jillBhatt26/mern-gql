11160371eb4dbb33f3dc10bea0f10e749a121997
jill bhatt <bhattjill@gmail.com>
2025-03-08 16:09:12 +0530

    Completed LoginUser mutation tests

--- __tests__/mutation/users.test.js
+++ __tests__/mutation/users.test.js
@@ -1,4 +1,5 @@
 const request = require('supertest');
+const { hash: hashPassword } = require('argon2');
 const initExpressApolloApp = require('../../app');
 const { TEST_DB_URL } = require('../../config/env');
 const { API_URL, TOTAL_DOC_LIMIT } = require('../../config/constants');
@@ -8,6 +9,17 @@ const UserModel = require('../../models/User');
 describe('USERS MUTATION SUITE', () => {
     let conn;
     let app;
+    const generateUserToSignup = async () => {
+        const hashedPassword = await hashPassword('password1');
+
+        const userToSignup = {
+            username: 'user1',
+            email: 'user1@email.com',
+            password: hashedPassword
+        };
+
+        return userToSignup;
+    };
 
     beforeAll(async () => {
         conn = await connectMongoDB(TEST_DB_URL);
@@ -197,8 +209,6 @@ describe('USERS MUTATION SUITE', () => {
                 .send({ query })
                 .set('Cookie', cookie);
 
-            console.log('body: ', secondSignUpRes.body);
-
             expect(secondSignUpRes.status).toStrictEqual(200);
             expect(secondSignUpRes.body.data).toHaveProperty('SignupUser');
             expect(secondSignUpRes.body.data.SignupUser).toBeNull();
@@ -214,7 +224,213 @@ describe('USERS MUTATION SUITE', () => {
     });
 
     describe('MUTATION LoginUser', () => {
-        it('Should log an existing user in', async () => {});
+        let userToSignup;
+
+        beforeEach(async () => {
+            userToSignup = await generateUserToSignup();
+
+            await UserModel.create(userToSignup);
+        });
+
+        it('Should not perform login with missing values', async () => {
+            const query = `
+                mutation Login {
+                    LoginUser (loginUserInput: { username: "" }) {
+                        _id,
+                        username,
+                        email
+                    }
+                }
+            `;
+
+            const response = await request(app).post(API_URL).send({ query });
+
+            expect(response.status).toStrictEqual(400);
+            expect(response.body).toHaveProperty('errors');
+            expect(response.body.data).toBeUndefined();
+            expect(response.body.errors).toStrictEqual(
+                expect.arrayContaining([
+                    {
+                        message:
+                            'Field "LoginUserInputType.password" of required type "String!" was not provided.'
+                    }
+                ])
+            );
+        });
+
+        it('Should not perform login with empty values', async () => {
+            const query = `
+                mutation Login {
+                    LoginUser (loginUserInput: { username: "", password: "password1" }) {
+                        _id,
+                        username,
+                        email
+                    }
+                }
+            `;
+
+            const response = await request(app).post(API_URL).send({ query });
+
+            expect(response.status).toStrictEqual(200);
+            expect(response.body.data).toHaveProperty('LoginUser');
+            expect(response.body.data.LoginUser).toBeNull();
+            expect(response.body).toHaveProperty('errors');
+            expect(response.body.errors).toStrictEqual(
+                expect.arrayContaining([
+                    {
+                        message: 'Either username or email is required to login'
+                    }
+                ])
+            );
+        });
+
+        it('Should validate if user exists', async () => {
+            const query = `
+                mutation Login {
+                    LoginUser (loginUserInput: { username: "user2", password: "password1" }) {
+                        _id,
+                        username,
+                        email
+                    }
+                }
+            `;
+
+            const response = await request(app).post(API_URL).send({ query });
+
+            expect(response.status).toStrictEqual(200);
+            expect(response.body.data).toHaveProperty('LoginUser');
+            expect(response.body.data.LoginUser).toBeNull();
+            expect(response.body).toHaveProperty('errors');
+            expect(response.body.errors).toStrictEqual(
+                expect.arrayContaining([
+                    {
+                        message: 'User not found! You need to signup first!'
+                    }
+                ])
+            );
+        });
+
+        it('Should validate if password is correct', async () => {
+            const query = `
+                mutation Login {
+                    LoginUser (loginUserInput: { username: "user1", password: "password2" }) {
+                        _id,
+                        username,
+                        email
+                    }
+                }
+            `;
+
+            const response = await request(app).post(API_URL).send({ query });
+
+            expect(response.status).toStrictEqual(200);
+            expect(response.body.data).toHaveProperty('LoginUser');
+            expect(response.body.data.LoginUser).toBeNull();
+            expect(response.body).toHaveProperty('errors');
+            expect(response.body.errors).toStrictEqual(
+                expect.arrayContaining([
+                    {
+                        message: 'Incorrect password!'
+                    }
+                ])
+            );
+        });
+
+        it('Should log an existing user in', async () => {
+            const query = `
+                mutation LoginUser {
+                    LoginUser (loginUserInput: { username: "user1", password: "password1"}) {
+                        _id,
+                        username,
+                        email
+                    }
+                }
+            `;
+
+            expect(userToSignup).toBeDefined();
+
+            const response = await request(app).post(API_URL).send({ query });
+
+            // status assertions
+            expect(response.status).toStrictEqual(200);
+
+            // body assertions
+            expect(response.body).toHaveProperty('data');
+            expect(response.body.errors).toBeUndefined();
+            expect(response.body.data).toHaveProperty('LoginUser');
+            expect(response.body.data.LoginUser).toHaveProperty('_id');
+            expect(response.body.data.LoginUser).not.toHaveProperty('password');
+
+            const { username, email } = response.body.data.LoginUser;
+
+            expect(username).toStrictEqual(userToSignup.username);
+            expect(email).toStrictEqual(userToSignup.email);
+
+            // headers assertions for cookies
+            expect(response.headers['set-cookie']).toBeDefined();
+            expect(response.headers['set-cookie'][0]).toContain('connect.sid');
+        });
+
+        it('Should not log in an already authenticated user', async () => {
+            let cookie;
+
+            const query = `
+                mutation LoginUser {
+                    LoginUser (loginUserInput: { username: "user1", password: "password1"}) {
+                        _id,
+                        username,
+                        email
+                    }
+                }
+            `;
+
+            expect(userToSignup).toBeDefined();
+
+            const firstLoginRes = await request(app)
+                .post(API_URL)
+                .send({ query });
+
+            // status assertions
+            expect(firstLoginRes.status).toStrictEqual(200);
+
+            // body assertions
+            expect(firstLoginRes.body).toHaveProperty('data');
+            expect(firstLoginRes.body.errors).toBeUndefined();
+            expect(firstLoginRes.body.data).toHaveProperty('LoginUser');
+            expect(firstLoginRes.body.data.LoginUser).toHaveProperty('_id');
+            expect(firstLoginRes.body.data.LoginUser).not.toHaveProperty(
+                'password'
+            );
+
+            const { username, email } = firstLoginRes.body.data.LoginUser;
+
+            expect(username).toStrictEqual(userToSignup.username);
+            expect(email).toStrictEqual(userToSignup.email);
+
+            // headers assertions for cookies
+            expect(firstLoginRes.headers['set-cookie']).toBeDefined();
+            expect(firstLoginRes.headers['set-cookie'][0]).toContain(
+                'connect.sid'
+            );
+
+            cookie = firstLoginRes.headers['set-cookie'];
+
+            const secondLoginRes = await request(app)
+                .post(API_URL)
+                .send({ query })
+                .set('Cookie', cookie);
+
+            expect(secondLoginRes.status).toStrictEqual(200);
+            expect(secondLoginRes.body).toHaveProperty('data');
+            expect(secondLoginRes.body.data).toHaveProperty('LoginUser');
+            expect(secondLoginRes.body.data.LoginUser).toBeNull();
+            expect(secondLoginRes.body).toHaveProperty('errors');
+            expect(secondLoginRes.body.errors).toStrictEqual(
+                expect.arrayContaining([
+                    { message: 'You are already logged in!' }
+                ])
+            );
+        });
     });
 
     describe('MUTATION LogoutUser', () => {
@@ -234,7 +450,7 @@ describe('USERS MUTATION SUITE', () => {
     });
 
     afterAll(async () => {
-        // await conn.connection.dropCollection('users');
+        await conn.connection.dropCollection('users');
         await conn.connection.dropDatabase();
         await disconnectMongoDB();
     });
