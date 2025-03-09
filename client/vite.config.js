27a34e5273a56de93ac254d8bc06cb4e645544b8
jill bhatt <bhattjill@gmail.com>
2025-03-10 01:45:31 +0530

    Setup vite react client

---
+++ client/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const port = process.env.VITE_PORT || 3000;
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        port
+    }
+});
