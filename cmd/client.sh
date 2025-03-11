a02cfee2d441088ef7e618b5b5e0344074ad8f68
jill bhatt <bhattjill@gmail.com>
2025-03-12 02:21:33 +0530

    Updated shell commands for client.sh #3

--- cmd/client.sh
+++ cmd/client.sh
@@ -1,7 +1,9 @@
 cd client
+touch .env
+echo VITE_PORT = $VITE_PORT >> .env
+echo VITE_NODE_ENV = $VITE_NODE_ENV >> .env
+echo VITE_BE_URL = $RENDER_EXTERNAL_HOSTNAME >> .env
+echo ".env generated"
 ls
-echo "VITE_PORT=$VITE_PORT" >> .env
-echo "VITE_NODE_ENV=$VITE_NODE_ENV" >> .env
-echo "VITE_BE_URL=$RENDER_EXTERNAL_HOSTNAME" >> .env
-ls
-cd client && yarn install --frozen-lockfile && yarn build
+yarn install --frozen-lockfile
+yarn build
