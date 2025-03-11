6e77ca5b7847c3cd0115295eae02e411cc523e32
jill bhatt <bhattjill@gmail.com>
2025-03-12 01:37:26 +0530

    Updated vite.config.js and removed dotenv imports

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import dotenv from 'dotenv';
-dotenv.config();
 
 const port = process.env.VITE_PORT || 3000;
 const BE_URL = process.env.VITE_BE_URL || `http://localhost:${port}/graphql`;
