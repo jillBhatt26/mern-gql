635b93a997f9fb726b3e8126ea25c8d5ee29f6d8
jill bhatt <bhattjill@gmail.com>
2025-03-12 02:30:01 +0530

    Updated client.sh #5

--- cmd/client.sh
+++ cmd/client.sh
@@ -5,5 +5,6 @@ echo VITE_NODE_ENV = $VITE_NODE_ENV >> .env
 echo VITE_BE_URL = $RENDER_EXTERNAL_HOSTNAME >> .env
 echo ".env generated"
 ls
-yarn install
+yarn install --frozen-lockfile
+yarn add vite@latest
 yarn build
