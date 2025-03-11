c48e972c98b9e3f780990563db2378279e3c85da
jill bhatt <bhattjill@gmail.com>
2025-03-12 02:42:15 +0530

    Updated client.sh #7

--- cmd/client.sh
+++ cmd/client.sh
@@ -1,7 +1,12 @@
 cd client
-echo VITE_PORT = $VITE_PORT >> .env
-echo VITE_NODE_ENV = $VITE_NODE_ENV >> .env
-echo VITE_BE_URL = $RENDER_EXTERNAL_HOSTNAME >> .env
+echo "Start print variables"
+echo "VITE_PORT = $VITE_PORT"
+echo "VITE_NODE_ENV = $VITE_NODE_ENV"
+echo "VITE_BE_URL = $RENDER_EXTERNAL_HOSTNAME"
+echo "End print variables"
+echo "VITE_PORT = $VITE_PORT" >> .env
+echo "VITE_NODE_ENV = $VITE_NODE_ENV" >> .env
+echo "VITE_BE_URL = $RENDER_EXTERNAL_HOSTNAME" >> .env
 ls
 yarn install --frozen-lockfile
 yarn build
