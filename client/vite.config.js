af558b10aa01bcb59d12016f977c6aac63e90150
jill bhatt <bhattjill@gmail.com>
2025-03-12 16:59:08 +0530

    Switching to conventional approach #1

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+import { resolve } from 'path';
+
+dotenv.config({ path: resolve(__dirname, '../.env') });
 
 const port = process.env.VITE_PORT || 3000;
-const BE_URL = process.env.VITE_BE_URL || `http://localhost:${port}/graphql`;
+const BE_URL = process.env.VITE_BE_URL || `http://localhost:5000/graphql`;
+
+console.log('VITE_PORT: ', process.env.VITE_PORT);
+console.log('BE_URL: ', process.env.VITE_BE_URL);
 
 // https://vite.dev/config/
 export default defineConfig({
