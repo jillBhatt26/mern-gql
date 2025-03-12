5f2e95b6ce101e9fde8de98a1a0e071988a2fd93
jill bhatt <bhattjill@gmail.com>
2025-03-12 15:45:08 +0530

    Tried different app session config

--- config/session/index.js
+++ config/session/index.js
@@ -2,6 +2,28 @@ const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const { DB_URL, SESSION_SECRET, NODE_ENV } = require('../env');
 
+// const initAppSession = (app, mongoUrl = DB_URL) => {
+//     const mongoStore = new MongoStore({
+//         collectionName: 'sessions',
+//         mongoUrl
+//     });
+
+//     const appSession = session({
+//         secret: SESSION_SECRET,
+//         saveUninitialized: false,
+//         resave: true,
+//         store: mongoStore,
+//         cookie: {
+//             maxAge: 1000 * 60 * 60 * 24, // 24 hours
+//             sameSite: NODE_ENV === 'production' ? 'none' : 'lax',
+//             httpOnly: NODE_ENV === 'production',
+//             secure: NODE_ENV === 'production' && app.get('trust proxy') === 1
+//         }
+//     });
+
+//     return appSession;
+// };
+
 const initAppSession = (app, mongoUrl = DB_URL) => {
     const mongoStore = new MongoStore({
         collectionName: 'sessions',
@@ -14,10 +36,10 @@ const initAppSession = (app, mongoUrl = DB_URL) => {
         resave: true,
         store: mongoStore,
         cookie: {
-            maxAge: 1000 * 60 * 60 * 24, // 24 hours
+            // maxAge: 1000 * 60 * 60 * 24, // 24 hours
             sameSite: NODE_ENV === 'production' ? 'none' : 'lax',
             httpOnly: NODE_ENV === 'production',
-            secure: NODE_ENV === 'production' && app.get('trust proxy') === 1
+            secure: NODE_ENV === 'production'
         }
     });
 
