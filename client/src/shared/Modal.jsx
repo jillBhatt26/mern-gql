015079d706e8b2940711aba4fd1486b21922b2ab
jill bhatt <bhattjill@gmail.com>
2025-03-16 14:13:32 +0530

    Added custom modal component and removed @headlessui/react

--- client/src/shared/Modal.jsx
+++ client/src/shared/Modal.jsx
@@ -1,30 +1,36 @@
-import { Dialog } from '@headlessui/react';
+import { createPortal } from 'react-dom';
 
 const Modal = ({ isOpen, onClose }) => {
-    return (
-        <Dialog open={isOpen} onClose={onClose} as="div">
-            <div className="dialog-overlay">
-                <div className="dialog-container">
-                    <div className="modal-header d-flex justify-content-between">
-                        <h3 className="modal-title">Dialog Title</h3>
-                        <button
-                            type="button"
-                            className="btn-close"
-                            onClick={onClose}
-                        >
-                            <span aria-hidden="true"></span>
-                        </button>
-                    </div>
-                    <div className="modal-body py-1">Modal body</div>
-                    <div className="modal-footer gap-3">
-                        <button className="btn btn-primary">Confirm</button>
-                        <button className="btn btn-secondary" onClick={onClose}>
-                            Cancel
-                        </button>
+    return createPortal(
+        <>
+            {isOpen && (
+                <div className="dialog-overlay">
+                    <div className="dialog-container">
+                        <div className="modal-header d-flex justify-content-between">
+                            <h3 className="modal-title">Dialog Title</h3>
+                            <button
+                                type="button"
+                                className="btn-close"
+                                onClick={onClose}
+                            >
+                                <span aria-hidden="true"></span>
+                            </button>
+                        </div>
+                        <div className="modal-body py-1">Modal body</div>
+                        <div className="modal-footer gap-3">
+                            <button className="btn btn-primary">Confirm</button>
+                            <button
+                                className="btn btn-secondary"
+                                onClick={onClose}
+                            >
+                                Cancel
+                            </button>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </Dialog>
+            )}
+        </>,
+        document.body
     );
 };
 
